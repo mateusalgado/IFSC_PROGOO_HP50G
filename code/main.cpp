81d6f6d6932c5ddab58f7e2e301229036e9c0f92
Pedro Daguer <pelajo.daguer@gmail.com>
2025-10-14 13:15:38 -0300

    Settings serializer

---
+++ code/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+typedef struct
+{
+    unsigned charCount;
+    char *brokerAddr;
+} HEADER;
+
+typedef struct
+{
+    unsigned ID;
+} STATION;
+
+class Settings
+{
+public:
+    Settings()
+    {
+        // Create file if does not exist.
+        file.open("settings.sema", std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+        if (!file.is_open())
+        {
+            printf_s("Falha ao abrir/criar configurações.\n");
+            return;
+        }
+        file.close();
+
+        // Open with read & write at any pos.
+        file.open("settings.sema", std::ios::binary | std::ios::in | std::ios::out);
+        if (!file.is_open())
+        {
+            printf_s("Falha ao abrir/criar configurações.\n");
+            return;
+        }
+    };
+
+    void save()
+    {
+        // Move to the beginning  of the file.
+        file.seekp(0, std::ios::beg);
+
+        // Write brokerAddr char count.
+        unsigned len = brokerAddr.length();
+        file.write(reinterpret_cast<char *>(&len), sizeof(len));
+
+        // Write brokkerAddr.
+        file.write(brokerAddr.c_str(), sizeof(char) * len);
+
+        // Write registered station's ID.
+        if (!stations.empty())
+            for (auto &station : stations)
+                file.write(reinterpret_cast<char *>(&station), sizeof(station));
+    };
+
+    void load()
+    {
+        // Move to the beginning  of the file.
+        file.seekg(0, std::ios::beg);
+
+        // Grab brokerAddr char count;
+        unsigned chars;
+        file.read(reinterpret_cast<char *>(&chars), sizeof(chars));
+
+        // Read brokerAddr chars to a payload buffer.
+        std::vector<char> buffer(chars);
+        file.read(buffer.data(), chars);
+
+        // Dump payload into brokerAddr;
+        brokerAddr.assign(buffer.data(), chars);
+    };
+
+    void close()
+    {
+        file.close();
+    }
+
+    std::string brokerAddr;
+    std::vector<unsigned> stations;
+
+    bool isFileEmpty()
+    {
+        return (fileSize() == 0);
+    }
+
+private:
+    std::streampos fileSize()
+    {
+        // Save cur pos;
+        auto fpos = file.tellg();
+        // Goto end of file;
+        file.seekg(0, std::ios::end);
+        // Read pos;
+        std::streampos s = file.tellg();
+        // Seek back to saved pos;
+        file.seekg(fpos);
+        return s;
+    }
+
+    std::fstream file;
+
+private:
+};
+
+int main()
+{
+    Settings settings;
+    if (settings.isFileEmpty())
+    {
+        //TODO: Configurar via UI;
+        printf_s("Configuração em branco.\n");
+        settings.brokerAddr = "teste";
+        settings.save();
+    }
+
+    settings.load();
+    printf_s("BROKER ADDR: %s\n", settings.brokerAddr.c_str());
+    settings.close();
+}
